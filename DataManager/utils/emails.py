d414ed0b28a7f15e614344cd4aa70c64d35e54b4
汪银光 <wangyg@gegejia.com>
2018-10-15 17:39:36 +0800

    Email

--- DataManager/utils/emails.py
+++ DataManager/utils/emails.py
@@ -13,6 +13,7 @@ def send_password(receiver, password):
         smtp_server = 'smtp.exmail.qq.com'
 
     subject = "质量中心新密码"
+    smtpPort = '465'
 
     body = MIMEText("您的新密码：" + password + "，请查收，谢谢！", _subtype='html', _charset='gb2312')
 
@@ -23,12 +24,10 @@ def send_password(receiver, password):
     msg.attach(body)
 
     try:
-        smtp = smtplib.SMTP_SSL()
+        smtp = smtplib.SMTP_SSL(smtp_server,smtpPort)
         smtp.connect(smtp_server)
         smtp.starttls()
         smtp.login(EMAIL_SEND_USERNAME, EMAIL_SEND_PASSWORD)
-
-
         smtp.sendmail(EMAIL_SEND_USERNAME, receiver.split(','), msg.as_string())
         smtp.quit()
     except smtplib.SMTPException:
